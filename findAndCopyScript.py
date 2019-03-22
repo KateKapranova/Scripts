1006e90884d022f113ecaf8a0d26e37a0fb6dd7f
Unknown <fiona-ea@yandex.by>
2019-03-22 22:21:07 +0100

    Commit from 22.03.

---
+++ findAndCopyScript.py
@@ -0,0 +1,19 @@
+#script to iterate over files in subdirectories to find specific file names
+#files satisfying the search query are copied into the destination folder
+#for Windows OS
+
+import os
+from shutil import copy
+
+
+directory="C:\\Users\\Katiebear\\Documents\\Studium\\5. Semester\\R_Practice\\KDEF_and_AKDEF\\KDEF"
+dst= "C:\\Users\\Katiebear\\Documents\\Studium\\5. Semester\\R_Practice\\KDEF_and_AKDEF\\KDEF\\Front"
+
+for subdir in os.listdir(directory):
+    if subdir != "Front":
+        for file in os.listdir(os.path.join(directory,subdir)):
+            if file.endswith("S.JPG"):
+                source=os.path.join(directory,subdir,file)
+                copy(source,dst)
+            else:
+                continue
